4f5f995f643138adb5806eab2beef36c6106e1a4
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-05-14 20:36:39 -0500

    Delete src directory

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://6a01f74d0d92f63dd2531d8e.mockapi.io/api/v1',
-  sensorsEndpoint: '/sensors',
-  alertsEndpoint: '/alerts',
-  subscriptionApiUrl: 'https://69fb530188a7af0ecca8fada.mockapi.io/api/v1',
-  subscriptionEndpoint: '/subscription',
-  plansEndpoint: '/plans'
-};
