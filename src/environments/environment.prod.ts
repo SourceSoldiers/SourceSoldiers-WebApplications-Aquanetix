100009c1d55c8480f0cc9b35e3c4a1906a69d255
DeterminedSoul7 <u202411402@upc.edu.pe>
2026-06-20 18:58:52 -0500

    chore: add production environment variables.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://aquanetix-platform-vwyl.onrender.com',
+    sensorsEndpoint: '/sensors',
+    alertsEndpoint: '/alerts',
+    subscriptionApiUrl: 'https://69fb530188a7af0ecca8fada.mockapi.io/api/v1',
+    subscriptionEndpoint: '/subscription',
+    plansEndpoint: '/plans'
+};
