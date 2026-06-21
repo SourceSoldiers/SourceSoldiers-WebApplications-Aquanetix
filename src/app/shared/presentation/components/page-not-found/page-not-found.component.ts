2a485b6858147e0ebc2a4f3ab34f0988aeebfa73
DeterminedSoul7 <u202411402@upc.edu.pe>
2026-06-20 19:26:31 -0500

    fix: fix routing for page not found components

--- src/app/shared/presentation/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/presentation/components/page-not-found/page-not-found.component.ts
@@ -13,7 +13,7 @@ import {TranslateModule} from "@ngx-translate/core";
       <mat-icon style="font-size:4rem; width:4rem; height:4rem; color:#64748B;">search_off</mat-icon>
       <h1 style="font-family:'Manrope',sans-serif; margin-top:16px;">{{ 'page-not-found.title' | translate }}</h1>
       <p style="color:#64748B;">{{ 'page-not-found.content' | translate }}</p>
-      <button mat-flat-button color="primary" (click)="router.navigate(['/monitoring/dashboard'])">
+      <button mat-flat-button color="primary" (click)="router.navigate(['/dashboard'])">
         {{ 'page-not-found.go-home' | translate }}
       </button>
     </div>
