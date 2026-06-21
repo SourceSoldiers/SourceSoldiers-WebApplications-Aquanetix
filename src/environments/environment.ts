dbced9a91d2fa932f0a45c33d03ebd0779f2e831
DeterminedSoul7 <u202411402@upc.edu.pe>
2026-06-20 19:31:28 -0500

    fix: retrieved mock api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://6a01f74d0d92f63dd2531d8e.mockapi.io/api/v1',
   sensorsEndpoint: '/sensors',
   alertsEndpoint: '/alerts',
   subscriptionApiUrl: 'https://69fb530188a7af0ecca8fada.mockapi.io/api/v1',
